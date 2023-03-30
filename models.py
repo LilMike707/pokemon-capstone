1233f63a578962273136e3cd3b0b58ef8ae61d81
LilMike707 <michaelmarracq@yahoo.com>
2023-03-29 21:45:47 -0700

    Update models.py

--- models.py
+++ models.py
@@ -33,5 +33,5 @@ class Like(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    card_id = db.Column(db.Integer, nullable=False)
+    card_id = db.Column(db.String, nullable=False)
     
